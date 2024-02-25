dcc4ff3c2a7ca021f1bea376783ff83027553ff9
Raihan Raju <idbraihan58@gmail.com>
2024-02-25 18:56:09 +0600

    updated

---
+++ preactice php/Demo1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php Document</title>
+</head>
+<body>
+    Hello PHP! <hr>
+    <?php
+     $name= array("jakir", "raju", "shakil"); 
+
+     foreach ($name as $x) {
+       echo "$x <br>";
+     }
+     ?>
+    
+</body>
+</html>
---
+++ preactice php/Demo2.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-practice1</title>
+</head>
+<body>
+    <h1>Hello php</h1> <hr>
+    <?php
+    //   $food="pizza";
+    //   echo"I love to eat ".$food."<br>";
+
+    //   $x=10;
+    //   $y=5;
+
+    //   echo"Total=".$x+$y."<br>";
+    //   echo"Subtract=".$x-$y."<br>";
+    //   echo"Multiplication=".$x*$y."<br>";
+    //   echo"Dividetion=".$x/$y."<br>";
+    //   echo"%=".$x%$y;
+
+
+//var_dump=Typeof....................
+
+    // $name="Raihan";
+    // $name1="Raihan Raju";
+    // $age=28;
+    // $age1=28.2;
+    // $relation=true;
+    // $chack=array("name","age","relation");
+
+    // var_dump($name)."<br>";
+    // var_dump($name1)."<br>";
+    // var_dump($age)."<br>";
+    // var_dump($age1)."<br>";
+    // var_dump($relation);
+    // var_dump($chack);
+
+//object oriented(oop).......................
+
+
+// class Phone{
+//     var $model;
+//     var $country;
+//     function PhoneFiture($number,$location){
+//         global $model;
+//         global $country;
+
+//         $model=$number;
+//         $country=$location;
+      
+
+//         echo "*This is $model, It's made by $country <br>";
+//     } 
+// }
+
+//   $apple = new phone;
+//   $apple->PhoneFiture(' iphon 13','USA');
+
+//   $Z_Book = new phone;
+//   $Z_Book->PhoneFiture(' Hp 14U','Germany');
+
+
+
+
+//math-mathicle..........max/ min.............
+
+//echo(max(21,4,55,666,87,9,));
+
+
+//possitive/Nagative.....rootnumber............
+
+//echo(abs(-66));
+
+
+//echo(sqrt(16));
+
+//echo(round(pi()));
+//echo(rand());
+//echo(rand(10,15));
+//echo(rand());
+
+
+
+//Faunction php.................
+// $date = 'H';
+
+// if(6>10){
+//     echo'Good Morning';
+// }
+// elseif(11<18){
+//     echo'Have a nice day!';
+// }
+// else'Good Night';
+
+
+                      //Strting function [strlen(),substr(),stroupper(),strtoupper(),strsupper()]
+
+//switch_case'''''''''''''''''''''''''
+
+// switch('green'){
+//     case 'red';
+//     echo'Apple is Red';
+//     break;
+
+//     case 'green';
+//     echo'Mango is Green';
+//     break;
+
+//     default;
+//     echo'I love to eat Pizza';
+// }
+
+
+
+//php loop..........While/ do-While/ For ..............
+
+// $x=1;
+// while($x <=10){
+//     echo"The number is: $x <br>";
+//     $x++;
+// }
+
+
+
+
+// $x=1;
+
+// do{
+//     echo"This number is: $x <br>";
+//     $x++;
+// }while($x <=10);
+
+
+
+
+
+for($x=0; $x <= 50; $x=$x+5){
+    echo"The number is: $x <br>";
+}
+
+
+
+
+
+
+
+
+
+
+    ?>
+</body>
+</html>
