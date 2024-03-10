c2528996b2df5d793b778ff06239803960300fdb
Raihan-Raju <idbraihan58@gmail.com>
2024-03-10 13:42:01 +0600

    oop for fainal

---
+++ preactice php/form-valid for final/indext2.php
@@ -0,0 +1,58 @@
+
+<?php
+    require_once("object2.php");
+
+    if (isset($_POST["btnSubmit"])){
+        $id=$_POST["txtId"];
+        $name=$_POST["txtName"];
+        $course=$_POST["txtCourse"];
+        $phone=$_POST["txtPhone"];
+
+             $student1=new Student($id,$name,$course,$phone);
+             $student1->save();
+
+        
+    }
+    
+    
+    ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>indext2-php</title>
+</head>
+<body>
+   
+
+
+ <h3 style= "text-align: center ";>PHP-- OOP oriented form</h3> <hr>
+          <form action="#" method="post">
+            
+                        <div style= "text-align: center ;background-color:#BBB3B2";>
+                            ID:<br/>
+                            <input type="number" name="txtId" /><br/>
+                            Name<br/>
+                            <input type="text" name="txtName" /><br/>
+                            Course<br/>
+                            <input type="text" name="txtCourse" /><br/>
+                            Phone<br/>
+                            <input type="number" name="txtPhone" /><br/><br/>
+                            <input type="submit" name="btnSubmit" value="Submit" />
+                        </div>
+           
+           </form>
+
+      <?php
+        Student::display_students();
+      ?>  
+      
+      
+     
+
+<footer style= "text-align: center ";><a href="login.php"><h1>Logout</h1></a></footer>
+</body>
+</html>
--- preactice php/form-valid for final/object2.php
+++ preactice php/form-valid for final/object2.php
@@ -1,22 +1,22 @@
-  <?php
+<?php
 class Student{
    private $id;
    private $name;
    private $coure;
-   
+   private $phone; 
 
    private static $file_path="data2.text";
 
-   function __construct($_id,$_name,$_coure){
+   function __construct($_id,$_name,$_coure,$_phone){
          $this->id=$_id;
          $this->name=$_name;
          $this->coure=$_coure;
-        
+         $this->phone=$_phone;
 
    }
 
         public function csv(){
-            return $this->id.",".$this->name.",". $this->coure.PHP_EOL;
+            return $this->id.",".$this->name.",". $this->coure.",".$this->phone.PHP_EOL;
         }
 
 
@@ -30,10 +30,10 @@ class Student{
    public static function display_students(){
       $students=file(self::$file_path);
 
-      echo "<b> ID |   Name     |    COURSE        </b> <br/>";
+      echo "<b> ID (Identyfication number) |   Name     |    COURSE   |    PHONE      </b> <br/>";
       foreach ($students as $student1) {
-        list($id,$name,$coure)=explode(",",trim($student1));
-        echo "$id | $name  | $coure  <br/>";
+        list($id,$name,$coure,$phone)=explode(",",trim($student1));
+        echo "$id | $name  | $coure | $phone <br/>";
       }
    }
 
