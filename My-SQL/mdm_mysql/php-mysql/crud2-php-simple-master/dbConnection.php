3f158d4bfd0408e345adcb7e4b60a5a2e5ceec44
Raihan-Raju <idbraihan58@gmail.com>
2024-05-02 16:36:53 +0600

    php mage

--- My-SQL/mdm_mysql/php-mysql/crud2-php-simple-master/dbConnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$databaseHost = 'localhost';
-$databaseName = 'my_First_Database';
-$databaseUsername = 'root';
-$databasePassword = '';
-
-// Open a new connection to the MySQL server
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
